cdc4840674f0f02a4beaaecbb79bfac3240951da
evgenyfokin <fokin_e@inbox.ru>
2023-07-17 12:38:01 +0300

    set up

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import mongoose from "mongoose";
 
+
+mongoose.connect('mongodb+srv://admin:Netflix2024@cluster0.fknd4ao.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {console.log('DB IS CONNECTED')})
+    .catch((err)=>{
+        console.log("DB IS FAILED", err)
+    })
 const app = express()
+app.use(express.json())
 const PORT = 4444
 app.get('/', (req, res) => {
     res.send("Hello world!")
 })
 
-app.listen(PORT, (e) => {
-    if (e) {
-        console.log(e)
+
+
+app.post('/auth/login', (req,res) => {
+    console.log(req.body)
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Fokin Eugene'
+    }, 'secret123')
+
+    res.json({
+        success: true,
+        token
+    })
+})
+
+
+
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err)
     }
     console.log("DB IS RUNNING")
 })
