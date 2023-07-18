6f97736d48204b1c1c0c2fd90cd696584611db42
evgenyfokin <fokin_e@inbox.ru>
2023-07-18 19:22:29 +0300

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import {loginValidation, registerValidation} from "./validations.js";
 import checkAuth from "./utils/auth.js";
 import {getMe, login, register} from "./controllers/UserController.js";
-
+import cors from 'cors'
 
 mongoose.connect(process.env.MONGODB_URI ||
     'mongodb+srv://admin:Netflix2024@cluster0.fknd4ao.mongodb.net/collections?retryWrites=true&w=majority')
@@ -15,7 +15,8 @@ mongoose.connect(process.env.MONGODB_URI ||
     })
 const app = express()
 app.use(express.json())
-const PORT = process.env.PORT || 4444
+app.use(cors())
+const PORT = process.env.PORT || 3004
 app.get('/', (req, res) => {
     res.send("Hello world!")
 })
