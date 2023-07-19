bb922cdcb34f382326b5a07e6d4d2f84fd17af58
evgenyfokin <fokin_e@inbox.ru>
2023-07-19 12:36:26 +0300

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import {loginValidation, registerValidation} from "./validations.js";
 import checkAuth from "./utils/auth.js";
 import {getMe, login, register} from "./controllers/UserController.js";
-// 'mongodb+srv://admin:Netflix2024@cluster0.fknd4ao.mongodb.net/collections?retryWrites=true&w=majority'
+import cors from "cors";
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('DB CONNECTED')
@@ -12,12 +12,9 @@ mongoose.connect(process.env.MONGODB_URI)
         console.log("DB IS FAILED", err)
     })
 const app = express()
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://ireact-ten.vercel.app");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    next();
-});
+app.use(cors({
+    origin: "https://ireact-ten.vercel.app"
+}));
 app.use(express.json())
 const PORT = process.env.PORT || 3004
 app.get('/', (req, res) => {
