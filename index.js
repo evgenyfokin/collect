4a5ff541ddf1e9aef25fedb00f1c434254a2577f
evgenyfokin <fokin_e@inbox.ru>
2023-07-19 16:02:44 +0300

    fix

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import {loginValidation, registerValidation} from "./validations.js";
 import checkAuth from "./utils/auth.js";
 import {getMe, login, register} from "./controllers/UserController.js";
 import cors from "cors";
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('DB CONNECTED')
@@ -12,6 +14,7 @@ mongoose.connect(process.env.MONGODB_URI)
         console.log("DB IS FAILED", err)
     })
 const app = express()
+
 app.use(cors({
     origin: 'https://ireact-ten.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -19,16 +22,30 @@ app.use(cors({
     preflightContinue: false,
     optionsSuccessStatus: 204
 }));
+
+
+app.use('/api', createProxyMiddleware({
+    target: 'https://fokin-collections-80e49a476c50.herokuapp.com', // Здесь ваша целевая ссылка
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api': '', // убираем /api из пути запроса
+    },
+}));
+
 app.use(express.json())
+
 const PORT = process.env.PORT || 3004
+
 app.get('/', (req, res) => {
     res.send("Hello world!")
 })
 
-
-app.post('/auth/register', registerValidation, register)
-app.post('/auth/login', loginValidation, login)
-app.get('/auth/me', checkAuth, getMe)
+// app.post('/auth/register', registerValidation, register)
+// app.post('/auth/login', loginValidation, login)
+// app.get('/auth/me', checkAuth, getMe)
+app.post('/api/auth/register', registerValidation, register)
+app.post('/api/auth/login', loginValidation, login)
+app.get('/api/auth/me', checkAuth, getMe)
 
 app.use(function(err, req, res, next) {
     console.error(err.stack); // Логирует стек вызова ошибки
